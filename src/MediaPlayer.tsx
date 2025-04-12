23230b9e766cbb4e58842f33733ca1f4f60f0dcf
VidaMaleki <vida.basmani@gmail.com>
2025-04-11 23:54:38 -0700

    Fixed warning issue for deployment

--- src/MediaPlayer.tsx
+++ src/MediaPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import playlistsData from "./data/playlists.json";
 import Playlist from "./components/PlayList/PlayList";
 import ArtistCard from "./components/ArtistCard/ArtistCard";
